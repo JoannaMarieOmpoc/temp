8c129e556493171bbae0f38b8d03201c3da7cf46
Joanna Marie <ompoc@myeskwela.msuiit.edu.ph>
2016-12-13 20:14:01 +0800

    NEW! SP for updating top employee entry

--- employees.sql
+++ employees.sql
@@ -2,21 +2,25 @@ CREATE TABLE topemployee(
   employeeID int8 PRIMARY KEY,
   employeename text,
   gender text,
+  salary text,
   address text
   )
 
 AS BEGIN
   IF @StatementType = 'Insert'
   BEGIN
-  insert into employee(employeeID, employeename, gender, address) values(@employeeID, @employeename, @gender, @address)
+  insert into employee(employeeID, employeename, gender, salary, address) values(@employeeID, @employeename, @gender, @salary, @address)
   END
 
   IF @StatementType = 'Update'
   BEGIN
 
-  UPDATE employee SET
-              First_name =  @first_name, last_name = @last_name, salary = @salary,
-              city = @city
-        WHERE id = @id
+  UPDATE employee
+    SET
+      employeename =  @employeename,
+      gender = @gender,
+      salary = @salary,
+      address = @address,
+    WHERE employeeID = @employeeID
   END
 end
