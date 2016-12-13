9241aa2f86efecfacc8a5e73b1ace4e305f9b808
Joanna Marie <ompoc@myeskwela.msuiit.edu.ph>
2016-12-13 20:11:00 +0800

    NEW! SP for adding new top employee entry

--- employees.sql
+++ employees.sql
@@ -9,4 +9,14 @@ AS BEGIN
   IF @StatementType = 'Insert'
   BEGIN
   insert into employee(employeeID, employeename, gender, address) values(@employeeID, @employeename, @gender, @address)
-END
+  END
+
+  IF @StatementType = 'Update'
+  BEGIN
+
+  UPDATE employee SET
+              First_name =  @first_name, last_name = @last_name, salary = @salary,
+              city = @city
+        WHERE id = @id
+  END
+end
