39bef3f99fbe77fcfda9a2c814cf88ca08c2282b
Joanna Marie <ompoc@myeskwela.msuiit.edu.ph>
2016-12-13 21:08:52 +0800

    NEW! API inserted to popular employee

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ from flask_httpauth import HTTPBasicAuth
 import flask
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@app.route('/employee/<int:employeeID>/<string:employeename>/<string:gender>/<string:salary>/<string:address>', methods=['POST'])
+def insertemployee(employeeID, employeename, gender, salary, address):
+    res = spcall('Insert', (employeeID, employeename, gender, salary, address), True)
+
+
+if __name__ == "__main__":
+    app.run()
