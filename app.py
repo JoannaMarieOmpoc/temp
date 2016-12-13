585ce3bc138d64e9bf75f2257760a92c0aa26812
Joanna Marie <ompoc@myeskwela.msuiit.edu.ph>
2016-12-13 21:16:58 +0800

    NEW! API to retrieve popular employee with vote count

--- app.py
+++ app.py
@@ -5,14 +5,21 @@ import flask
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
-@app.route('/employee/<int:employeeID>/<string:employeename>/<string:gender>/<string:salary>/<string:address>', methods=['POST'])
-def insertemployee(employeeID, employeename, gender, salary, address):
-    res = spcall('Insert', (employeeID, employeename, gender, salary, address), True)
+@app.route('/employee/<int:employeeID>/<string:topemployeeID>/<string:employeename>/<string:gender>/<string:salary>/<string:address>', methods=['POST'])
+def insertemployee(employeeID, topemployeeID, employeename, gender, salary, address):
+    res = spcall('entries', (employeeID, topemployeeID, employeename, gender, salary, address), True)
 
-@app.route('/employee/<int:employeeID>/<string:employeename>/<string:gender>/<string:salary>/<string:address>', methods=['POST'])
-def updateemployee(employeeID, employeename, gender, salary, address):
-    res = spcall('Update', (employeeID, employeename, gender, salary, address), True)
+@app.route('/employee/<int:employeeID>/<string:topemployeeID>/<string:employeename>/<string:gender>/<string:salary>/<string:address>', methods=['POST'])
+def updateemployee(employeeID, topemployeeID, employeename, gender, salary, address):
+    res = spcall('entries', (employeeID, topemployeeID, employeename, gender, salary, address), True)
 
+@app.route('/employee', methods=['GET'])
+def viewemployee():
+    res = spcall('employee', ())
+
+@app.route('/employee', methods=['GET'])
+def viewemployee():
+    res = spcall('votecount', ())
 
 if __name__ == "__main__":
     app.run()
